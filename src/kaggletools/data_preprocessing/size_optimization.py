a34539ddca8887da05dcc5235174ce9130c3bc8c
VasiliyKirstia <vasiliykirstia@yandex.ru>
2022-06-14 22:38:19 +0300

    Started using another approach for types detection

--- src/kaggletools/data_preprocessing/size_optimization.py
+++ src/kaggletools/data_preprocessing/size_optimization.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas.api.types import infer_dtype
 import numpy as np
 
 
@@ -13,7 +14,57 @@ def optimize_dtypes(df: pd.DataFrame) -> None:
         return
 
     for column in df.columns:
-        column_type = df.dtypes[column]
+        column_type = infer_dtype(df[column], skipna=True)
+        has_na = df[column].isnull().sum() > 0
+
+        if column_type == 'floating' or (column_type == 'integer' and has_na) or column_type == 'mixed-integer-float':
+            max_value = df[column].abs().max()
+            max_value *= 1.99
+
+            new_type = np.min_scalar_type(max_value)
+            df[column] = df[column].astype(new_type)
+
+        elif column_type == 'integer':
+            max_value = df[column].abs().max()
+            if np.any(df[column] < 0):
+                max_value *= -2
+            else:
+                max_value *= 2
+
+            new_type = np.min_scalar_type(max_value)
+            df[column] = df[column].astype(new_type)
+
+        elif:
+
+
+
+
+
+
+
+        elif
+        'string'
+        'bytes'
+        ''
+        'mixed-integer'
+
+        'decimal'
+        'complex'
+        'categorical'
+        'boolean'
+        'datetime64'
+        'datetime'
+        'date'
+        'timedelta64'
+        'timedelta'
+        'time'
+        'period'
+        'mixed'
+        'unknown-array'
+
+
+
+         = df.dtypes[column]
 
         if column_type == 'object':
             n_items = len(df)

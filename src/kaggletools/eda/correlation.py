b35d01ef2c943b2f45d0fcd87f8a3e0d5ede1171
VasiliyKirstia <vasiliykirstia@yandex.ru>
2022-06-16 15:11:50 +0300

    Added function to display correlations table

--- src/kaggletools/eda/correlation.py
+++ src/kaggletools/eda/correlation.py
@@ -1,7 +1,10 @@
 from typing import Callable
 
-from kaggletools.core.visualisation.pandas import GradientBackgroundFiller
+from kaggletools.core.visualisation.pandas import GradientBackgroundFiller, NumericalDataTransformer
 import pandas as pd
+
+
+__all__ = ['correlation_heatmap']
 
 
 def correlation_heatmap(
@@ -10,7 +13,8 @@ def correlation_heatmap(
     display: Callable[[pd.DataFrame], None] = None,
 ) -> pd.DataFrame:
     corr = df.corr(method=method)
-    corr.style.pipe(GradientBackgroundFiller.red_blue)
+    corr.style.pipe(GradientBackgroundFiller.red_blue(vmin=-1, vmax=1))
+    corr.style.pipe(NumericalDataTransformer.round(precision=2, truncate_leading_zero=True))
 
     if display is not None:
         with pd.option_context(
